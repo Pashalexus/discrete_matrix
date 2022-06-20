4fdb1f57016646c1aa97d785a51824d0f5e190a4
Alex Pashkov <107796317+Pashalexus@users.noreply.github.com>
2022-06-20 18:28:22 +0300

    Update inc_adj_matrix.pas

--- inc_adj_matrix.pas
+++ inc_adj_matrix.pas
@@ -1,66 +1,74 @@
-﻿var c: array[1..7, 1..7] of Boolean;
-var d: array[1..7, 1..14] of ShortInt;
+const n = 7; // 
+var c: array[1..n, 1..n] of Boolean;
+var d: array[1..n, 1..n*n+n] of ShortInt;
 begin
   println('                  Министерство науки и высшего образования РФ                  ');
   println('                         ФГБОУ ВО РГРТУ им. В.Ф.Уткина                         ');
   println('                                Кафедра САПР ВС                                ');
   println('                                  Рязань 2022                                  ');
   println('Программа по переводу матрицы инцидентности графа в матрицу смежности и обратно');
-  for var j: integer:=1 to 14 do // ввод матрицы инцидентности
-    for var i: integer:=1 to 7 do
+  var k: byte:=0; // номер столбца матрицы инцидентности
+  var boo: boolean:=True;
+  for var j: integer:=1 to n*n+n do // ввод матрицы инцидентности
+  begin
+    for var i: integer:=1 to n do
     begin
+      inc(k);
       print('d [',i,',',j,'] =');
-      d[i,j]:=ReadInteger()
+      d[i,j]:=ReadInteger();
+      if (d[i,j]=3) then boo:=False;
+      if not boo then break; // при вводе "тройки" происходит прерывание
     end;
-  var k: byte:=1; // номер столбца матрицы инцидентности
-  for var i:Integer:=1 to 7 do // вычисление матрицы смежности
-    for var j:Integer:=1 to 7 do
-      for k:=1 to 14 do
-      begin
-        if (d[i,k]=2) then // петли
+    if not boo then break
+  end;
+  for var i:Integer:=1 to n do
+    for var j:Integer:=1 to n do
+      for var k1:Integer:=1 to k do
+      begin // вычисление матрицы смежности
+        if (d[i,k1]=2) then // петли
           c[i,i]:=True
-        else if (d[i,k]=1) and (d[j,k]=1) and (i<>j) then // рёбра
+        else if (d[i,k1]=1) and (d[j,k1]=1) and (i<>j) then // рёбра
         begin
           c[i,j]:=True;
           c[j,i]:=True
         end
-        else if (d[i,k]=1) and (d[j,k]=-1) and (i<>j) then // дуги
+        else if (d[i,k1]=1) and (d[j,k1]=-1) and (i<>j) then // дуги
           c[j,i]:=True
       end;
-  for var i: integer:=1 to 7 do // печать матрицы смежности
+  for var i: integer:=1 to n do // печать матрицы смежности
   begin
-    for var j: integer:=1 to 7 do
+    for var j: integer:=1 to n do
       if c[i,j] then print('1') else print('0');
     println
   end;
-  for var i: integer:=1 to 7 do // зануление матрицы инцидентности
-    for var j: integer:=1 to 14 do
+  for var i: integer:=1 to n do // зануление матрицы инцидентности
+    for var j: integer:=1 to k do
       d[i,j]:=0;
-  k:=1;
-  for var i: integer:=1 to 7 do
-    for var j: integer:=1 to 7 do
+  k:=0;
+  for var i: integer:=1 to n do
+    for var j: integer:=1 to n do
     begin // перевычисление матрицы инцидентности
       if c[i,j] and (i=j) then
       begin // петли
-        d[i,k]:=2;
         inc(k);
+        d[i,k]:=2;
       end
       else if c[i,j] and c[j,i] and (i<j) then
       begin // рёбра
-        d[i,k]:=1;
-        d[j,k]:=1;
         inc(k);
+        d[i,k]:=1;
+        d[j,k]:=1
       end;
       if c[i,j] and not c[j,i] then
       begin // дуги
-        d[i,k]:=-1;
-        d[j,k]:=1;
         inc(k);
+        d[i,k]:=-1;
+        d[j,k]:=1
       end
     end;
-  for var i: integer:=1 to 7 do
+  for var i: integer:=1 to n do
   begin
-    for var j: integer:=1 to 14 do
+    for var j: integer:=1 to k do
     begin // печать восстановленной матрицы инцидентности
       if d[i,j]>=0 then write(' ');
       print(d[i,j]);
